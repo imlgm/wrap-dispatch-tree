64e75c10c9c461dea6ecc211dd07ea8aea8d42ee
LiGuoming <it1011@qq.com>
2016-05-19 10:38:23 +0800

    修复es6在build下报错的问题

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var _ = require('lodash');
 function mapTreeDispatch(actionCreators) {
     return function(dispatch){
         const looper = function(obj){
-            _.forEach(obj, (val, key, thisObj)=>{
+            _.forEach(obj, function(val, key, thisObj){
                 if(typeof(val) === 'object') looper(val);
-                if(typeof(val)==='function') thisObj[key] = function(...args){
-                    dispatch(val(...args))
+                if(typeof(val)==='function') thisObj[key] = function(){
+                    dispatch(val.apply(this,arguments))
                 }
             })
         };
